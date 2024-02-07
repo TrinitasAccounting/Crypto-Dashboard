6d9f83f7db418c3c2fe119ffe35702cf9643fe9b
Clay Mangum <clay@trinitasaccounting.com>
2024-02-06 19:05:34 -0500

    completed functionality of watchlist and all assets. Major functionality

--- index.js
+++ index.js
@@ -7,22 +7,33 @@
 // const newULElement = document.createElement('li');
 // mainCryptoDashboard.appendChild(newULElement);
 
+
+
+
+
+//Setting global variables that I can access to filter from_____
+let currentWatchlistCoinsArray = [];
+
+
+
 const mainULElement = document.querySelector('.cards');
 
 fetch('http://localhost:3000/crypto')
     .then(res => res.json())
     .then(data => data.forEach((coin) => {
 
-        //creating the list element
+
+
+        //creating the list element________________________
         const newLiElement = document.createElement('li');
         newLiElement.className = 'card';
         mainULElement.appendChild(newLiElement);
 
-        //Creating the Div element
+        //Creating the Div element__________________________
         const newDivElement = document.createElement('div');
         newLiElement.appendChild(newDivElement);
 
-        //Creating all of the elements to add into this Div above
+        //Creating all of the elements to add into the Div above
         const cryptoNameElement = document.createElement('h3');
         const cryptoLogoElement = document.createElement('img');
         cryptoLogoElement.className = 'logo-list'
@@ -53,8 +64,137 @@ fetch('http://localhost:3000/crypto')
 
 
 
+        //Creating the button element to add to watchlist______
+        const btnElement = document.createElement('button');
+        btnElement.textContent = 'Watchlist';
+        btnElement.className = 'watchlist-button';
 
+        newDivElement.appendChild(btnElement);
+
+        //Button Event Listener for adding to the watchlist_____
+        btnElement.addEventListener('click', () => {
+            addingToTheWatchlistDB(coin)
+
+            // rerenderWatchlistData()
+        });
 
 
     }))
+
+
+
+
+
+//Creating the watchlist POST and DELETE _______________________________________________________________________________
+//______________________________________________________________________________________________________________________
+
+//GET request for watchlist and rendering the watchlist
+const watchlistUlElement = document.querySelector('.watchlist-cards');
+
+function rerenderWatchlistData() {
+
+    fetch('http://localhost:3000/watchlist')
+        .then(res => res.json())
+        .then(data => data.forEach((watchlistCoin) => {
+
+
+            //Checking the global scope array to see if the 'id' is already included. If so do nothing, if not then rerender the watchlist immediately
+            //variable is defined at the top of index.js
+            if (currentWatchlistCoinsArray.includes(watchlistCoin.id)) {
+
+            }
+            else {
+                //creating and attaching the <li> element to the div
+                const newWatchlistLiElement = document.createElement('li');
+                newWatchlistLiElement.className = 'watchlist-card';
+                watchlistUlElement.appendChild(newWatchlistLiElement);
+
+                //Creating the div to append to the list
+                const watchlistDivElement = document.createElement('div');
+                newWatchlistLiElement.appendChild(watchlistDivElement);
+
+                //Creating the Header elements to attach to the div
+                const watchlistLogoElement = document.createElement('img');
+                watchlistLogoElement.className = 'watchlist-logo';
+                watchlistLogoElement.src = watchlistCoin.image;
+                const watchlistNameElement = document.createElement('h3');
+                watchlistNameElement.className = 'watchlist-card-name';
+                watchlistNameElement.textContent = watchlistCoin.name;
+                const watchlistPriceElement = document.createElement('h3');
+                watchlistPriceElement.className = 'watchlist-card-price';
+                watchlistPriceElement.textContent = `${watchlistCoin.current_price}`;
+                const watchlistPriceHardCodedElement = document.createElement('h5');
+                watchlistPriceHardCodedElement.className = 'watchlist-price-hardcoded'
+                watchlistPriceHardCodedElement.textContent = 'Price:';
+
+                //Delete button for each watchlist item_______________________
+                const watchlistItemDeleteBtn = document.createElement('button');
+                watchlistItemDeleteBtn.textContent = 'X';
+                watchlistItemDeleteBtn.className = 'watchlist-delete-button';
+                //Delete button click listener to DELETE request and to remove from global array
+                watchlistItemDeleteBtn.addEventListener('click', () => {
+                    deleteFromTheWatchlist(watchlistCoin)
+                })
+
+
+                //Appending headers to the div element_________________
+                watchlistDivElement.appendChild(watchlistLogoElement);
+                watchlistDivElement.appendChild(watchlistNameElement);
+                watchlistDivElement.appendChild(watchlistPriceElement);
+                watchlistDivElement.appendChild(watchlistItemDeleteBtn);
+                watchlistDivElement.appendChild(watchlistPriceHardCodedElement);
+
+
+                //Adding the coin to the watchlist array______________________________________****
+                currentWatchlistCoinsArray.push(watchlistCoin.id);
+            };
+
+
+        }))
+};
+
+rerenderWatchlistData();
+
+console.log(currentWatchlistCoinsArray);
+
+
+
+//POST Function
+function addingToTheWatchlistDB(coinObj) {
+    fetch('http://localhost:3000/watchlist', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application.json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            "id": coinObj.id,
+            "symbol": coinObj.symbol,
+            "name": coinObj.name,
+            "image": coinObj.image,
+            "current_price": coinObj.current_price,
+            "market_cap": coinObj.market_cap,
+            "total_volume": coinObj.total_volume,
+            "shares_owned": 0
+        })
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+};
+
+
+//DELETE Request Function 
+function deleteFromTheWatchlist(coinObj) {
+    fetch(`http://localhost:3000/watchlist/${coinObj.id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+};
+
+
 
